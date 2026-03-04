ee48dac2cd100ca1057f74b8247fe5caa232d663
Rhyan Clark Randriamisaina <rhrandri@c1r13s16.42antananarivo.mg>
2026-03-04 14:17:28 +0300

    push_swap.h

---
+++ rotate.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhrandri <rhrandri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/04 13:05:01 by rhrandri          #+#    #+#             */
+/*   Updated: 2026/03/04 14:02:37 by rhrandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap"
+
+void	rotate(t_stack **head)
+{
+	t_stack	*tail;
+
+	if (!*head || !(*head)->next)
+		return ;
+	tail = head;
+	while (tail->next != NULL) 
+		tail = tail->next;
+	tail->next = *head;
+	*head = (*head->next);
+	tail->next->next = NULL;
+}
+
+void	ra(t_stack **a)
+{
+	rotate(a);
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_stack **b)
+{
+	rotate(b);
+	write(1, "rb\n", 3);
+}
+
+void	rr(t_stack **a, t_stack **b)
+{
+	rotate(a);
+	rotate(b);
+	write(1, "rr\n", 3);
+}
